c182e84270eb8a0645dcfbc4d36350ca6d70d4a5
“Nicolas Alexandru Patrut” <nicolas.patrut.external@atos.net>
2023-06-18 19:26:56 +0200

    onboarding added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'auth/main_page.dart';
+import 'package:flutter/services.dart';
+import 'onboarding/onboarding_screen.dart';
 //import 'onboarding_screen.dart';
 
 void main() async {
@@ -8,18 +9,22 @@ void main() async {
 
   await Firebase.initializeApp();
 
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: Onboarding_screen(),
     );
   }
 }
---
+++ lib/onboarding/onboarding_screen.dart
@@ -0,0 +1,157 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../auth/main_page.dart';
+import 'content_model.dart';
+
+class Onboarding_screen extends StatefulWidget {
+  const Onboarding_screen({super.key});
+
+  @override
+  State<Onboarding_screen> createState() => _Onboarding_screenState();
+}
+
+class _Onboarding_screenState extends State<Onboarding_screen> {
+  int currentIndex = 0;
+
+  PageController _controller = PageController(initialPage: 0);
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+//If the user has already seen it, it won't be shown again.
+  _storeOnboardingInformation() async {
+    int isViewed = 0;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              //controller
+              controller: _controller,
+              itemCount: contents.length,
+              onPageChanged: (int index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              itemBuilder: (_, i) {
+                return Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Column(
+                    children: [
+                      SvgPicture.asset(
+                        contents[i].image,
+                        height: 300,
+                      ),
+                      Text(
+                        contents[i].title,
+                        style: const TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        contents[i].description,
+                        //For centering the text box to the center
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              contents.length,
+              (index) => buildDot(index, context),
+            ),
+          ),
+          Container(
+            height: 55,
+            margin: const EdgeInsets.all(40),
+            width: double.infinity,
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.purple[100];
+                    }
+                    return null; // Defer to the widget's default.
+                  },
+                ),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: const BorderSide(
+                      color: Colors.pink,
+                      width: 5,
+                    ),
+                  ),
+                ),
+              ),
+              child: Text(
+                currentIndex == contents.length - 1 ? "Continuar" : "Siguiente",
+                style: const TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              onPressed: () {
+                if (currentIndex == contents.length - 1) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const MainPage(),
+                    ),
+                  );
+                }
+                _controller.nextPage(
+                  duration: const Duration(milliseconds: 100),
+                  curve: Curves.bounceIn,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+      height: 10,
+      //we apply more width if we are in the current index number
+      width: currentIndex == index ? 20 : 10,
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.pink,
+      ),
+    );
+  }
+}
